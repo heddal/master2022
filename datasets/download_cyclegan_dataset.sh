ff90662c54b50050646696ea6ed4e9a73166e02b
Hedda Langvik <hedda.langvik@gmail.com>
2022-03-28 12:13:47 +0200

    Add no face download

--- datasets/download_cyclegan_dataset.sh
+++ datasets/download_cyclegan_dataset.sh
@@ -1,5 +1,16 @@
 FILE=$1
 
+if [[ $FILE == No_face ]]; then
+    echo "Specified [$FILE]"
+    URL=https://drive.google.com/file/d/1shf3KbSqHBsWRP6unO3qlZFMVITHjn8y/view?usp=sharing
+    ZIP_FILE=./$FILE
+    TARGET_DIR=./datasets/$FILE/
+    wget -N $URL -O $ZIP_FILE
+    mkdir $TARGET_DIR
+    unzip $ZIP_FILE -d ./datasets/
+    rm $ZIP_FILE
+fi
+
 if [[ $FILE != "ae_photos" && $FILE != "apple2orange" && $FILE != "summer2winter_yosemite" &&  $FILE != "horse2zebra" && $FILE != "monet2photo" && $FILE != "cezanne2photo" && $FILE != "ukiyoe2photo" && $FILE != "vangogh2photo" && $FILE != "maps" && $FILE != "cityscapes" && $FILE != "facades" && $FILE != "iphone2dslr_flower" && $FILE != "mini" && $FILE != "mini_pix2pix" && $FILE != "mini_colorization" ]]; then
     echo "Available datasets are: apple2orange, summer2winter_yosemite, horse2zebra, monet2photo, cezanne2photo, ukiyoe2photo, vangogh2photo, maps, cityscapes, facades, iphone2dslr_flower, ae_photos"
     exit 1
