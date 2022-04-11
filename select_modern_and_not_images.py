b9dbaab33e81d3e7c36800f18c3d7de977d2b4b6
Hedda Langvik <hedda.langvik@gmail.com>
2022-04-11 17:01:35 +0200

    chose modern and not

--- select_modern_and_not_images.py
+++ select_modern_and_not_images.py
@@ -5,11 +5,14 @@ import pandas as pd
 
 if __name__ == '__main__':
     folder = sys.argv[1]
-    i=0
-    wikiart_info = pd.read_csv(
-    while i!==2:
-        
+    a = 0
+    b = 0
+    wikiart_info = pd.read_csv('data/WikiArt-Ag4-cleaned.tsv', sep='\t')
+    while a != 1 & b != 1:
         image_name = random.choice(os.listdir(folder))
-        if "real" in  image_nam
-        print("The chosen modern image for this song is", image_name)
-        
+        if wikiart_info[(wikiart_info['Title'] == image_name) & (wikiart_info['Style'] == "Modern Art")] & a == 0:
+            print("The chosen modern image for this song is", image_name)
+            a += 1
+        if wikiart_info[(wikiart_info['Title'] == image_name) & (wikiart_info['Style'] != "Modern Art")] & b == 0:
+            print("The chosen non-modern image for this song is", image_name)
+            b += 1
