229066e8faed80f7d1415c91f2b66f9e6f684c28
Hedda Langvik <hedda.langvik@gmail.com>
2022-04-12 09:28:01 +0200

    pick two different

--- select_modern_and_not_images.py
+++ select_modern_and_not_images.py
@@ -4,17 +4,10 @@ import random
 import pandas as pd
 
 if __name__ == '__main__':
-    folder = sys.argv[1]
-    a = 0
-    b = 0
-    wikiart_info = pd.read_csv('data/WikiArt-Ag4-cleaned.tsv', sep='\t')
-    correct_style = wikiart_info.loc[wikiart_info['Style']
-                                     == 'Post Renaissance Art']
-    while a == 0 or b == 0:
-        image_name = random.choice(os.listdir(folder))
-        if (correct_style['Title'].apply(lambda x: True if correct_style['Image URL'].str.contains(image_name).any() else False).any()) and a == 0:
-            print("The chosen post renaissance image for this song is", image_name)
-            a += 1
-        elif b == 0:
-            print("The chosen general image for this song is", image_name)
-            b += 1
+    quadrant = sys.argv[1]
+    folder1 = "./datasets/All/"+quadrant
+    folder2 = "./datasets/No_face/testB/"+quadrant
+    image_name1 = random.choice(os.listdir(folder1))
+    image_name2 = random.choice(os.listdir(folder2))
+    print("The chosen general image for this song is", image_name1)
+    print("The chosen landscape image for this song is", image_name2)
